e57a78bd417174cbedebb9671c21b0f4fdf18aaf
vishal-1230 <vishalvishwajeet422@gmail.com>
2023-07-10 23:41:03 +0530

    .

--- layouts/chat-bot/LeftPanel.tsx
+++ layouts/chat-bot/LeftPanel.tsx
@@ -42,7 +42,7 @@ function LeftPanel(props: {mode: string, setMode: any, showPersonalBotDialog: bo
 
     async function getNotif() {
         setNotificationsLoading(true)
-        const response = await fetch("http://localhost:5000/get-connections", {
+        const response = await fetch("https://server.vikrambots.in/get-connections", {
             method: "GET",
             headers: {
                 "x-access-token": localStorage.getItem("token")!
